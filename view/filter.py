8f8f082d9e27ff879f738084c95ed7d0cdf64434
chuanjiabao <chuanjiabao1981@gmail.com>
2011-05-29 21:42:28 +0800

    replies

--- view/filter.py
+++ view/filter.py
@@ -1,7 +1,16 @@
+# coding=utf-8
 def linebreaksbr(text):
 	return text.replace("\r\n","<br/>").replace("\n","<br/>").replace("\r","<br>")
 def htmlspace(text):
 	return text.replace(" ","&nbsp;");
+## mako filter 带参数
+def avatar(arg):
+	def filter(text):
+    		default = "/static/img/avatar_" + str(arg) +".png"
+		return '<img src="' + default + '" border="0" />'  
+		return default
+	return filter
 if __name__=="__main__":
 	print linebreaksbr("a\r\nb")
 	print htmlspace("   ab  ")
+	print avatar("normal")("xx")
